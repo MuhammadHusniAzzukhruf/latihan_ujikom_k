64c0540ab502af2a927ae280828a9aed95c48757
Husni <husniazzukhruf@gmail.com>
2024-10-30 11:03:18 +0700

    mencoba github

---
+++ resources/views/home/user/edit.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.master')
+@section('tittle','user')
+@section('content')
+<div class="content-wrapper">
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>Halaman Edit Data User</h3>
+                            <a class="btn btn-warning" href="/user">Kembali</a>
+                        </div>
+                        <div class="card-body">
+                            <form action="/user/{{ $user->id }}/update" method="post">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Nama User</label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        name="name"
+                                        value="{{ $user->name }}"
+                                        id=""
+                                        aria-describedby="helpId"
+                                        placeholder=""
+                                    />
+                                    <small id="helpId" class="form-text text-muted">Help text</small>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Email</label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        name="email"
+                                        id=""
+                                        aria-describedby="helpId"
+                                        placeholder=""
+                                    />
+                                    <small id="helpId" class="form-text text-muted">Help text</small>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Password</label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        name="password"
+                                        id=""
+                                        value="{{ $user->password}}"
+                                        aria-describedby="helpId"
+                                        placeholder=""
+                                    />
+                                    <small id="helpId" class="form-text text-muted">Help text</small>
+                                </div>
+                                <button class="btn btn-primary" type="submit">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home.dashboard');
+});
+
+Route::get('/user',[UserController::class, 'index']);
+Route::get('/user/tambah',[UserController::class, 'create']);
+Route::post('/user/simpan',[UserController::class, 'store']);
+Route::get('/user/{id}/show',[UserController::class, 'show']);
+Route::post('/user/{id}/update',[UserController::class, 'update']);
+Route::get('/user/{id}/delete',[UserController::class, 'destroy']);
